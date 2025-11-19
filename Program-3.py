30e8801f5317dd68baade395fca102171ca494be
Leburu Devipriya <147066243+Devipriyaleburu@users.noreply.github.com>
2025-11-20 00:37:03 +0530

    Fix logic to append odd numbers up to input value

--- Program-3.py
+++ Program-3.py
@@ -1,9 +1,10 @@
+"""USING PYTHON LANGUAGE PROGRAM3"""
 a = int(input())
 if a % 2 != 0:
     t=a
 else:
     t=a - 1
 l = []
-for i in range(1, a+ 1, 2):
+for i in range(1, t+ 1, 2):
     l.append(i)
 print(l)
